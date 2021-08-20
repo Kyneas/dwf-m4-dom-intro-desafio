9a96559d9f60fa2b9f88c404ad74caaeca1ec40a
Matias Ferreira <ferre.matias@hotmail.com>
2021-08-20 14:44:24 -0300

    resuelto

--- index.js
+++ index.js
@@ -25,6 +25,19 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  const listaEls = document.querySelectorAll(".lista li");
+  for (let index = 0; index < listaEls.length; index++) {
+    listaEls[index].remove();
+  }
+
+  const listaEl = document.querySelector(".lista");
+  for (let index = 0; index < cosasQueAprendimos.length; index++) {
+    const newLiEl = document.createElement("li");
+    newLiEl.textContent = cosasQueAprendimos[index].tema;
+    newLiEl.classList.add(cosasQueAprendimos[index].class || "asd");
+    listaEl.appendChild(newLiEl);
+  }
+}
 
 main();
